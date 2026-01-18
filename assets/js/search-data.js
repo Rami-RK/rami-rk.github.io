56c02ab1283db8d349cc2334750f965e8bc67844
Rami-RK <50700579+Rami-RK@users.noreply.github.com>
2026-01-18 18:05:58 +0000

    Deploying to gh-pages from @ Rami-RK/rami-rk.github.io@54700e01080b5efd1c3baea83cf3f04bf5b7e3db 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,24 +17,13 @@ ninja.data = [{
             window.location.href = "/blog/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-",
-        
-          title: "",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2026/2025-03-26-self-attention/";
-          
-        },
-      },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
@@ -43,6 +32,17 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2025/plotly/";
+          
+        },
+      },{id: "post-self-attention",
+        
+          title: "Self Attention",
+        
+        description: "From intuition to scaled dot-product attention.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/self-attention/";
           
         },
       },{id: "post-a-post-with-image-galleries",
