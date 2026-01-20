84fb75c1dfab1837e10b65ba1a4c8906eca34934
Rami-RK <50700579+Rami-RK@users.noreply.github.com>
2026-01-20 19:56:49 +0000

    Deploying to gh-pages from @ Rami-RK/rami-rk.github.io@02ff199bccf32bd667f971332e77558e4b367608 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,4 +23,37 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },];
+        },{id: "post-self-attention",
+        
+          title: "Self Attention",
+        
+        description: "From intuition to scaled dot-product attention.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/self-attention/";
+          
+        },
+      },{id: "post-transformer-model-architecture",
+        
+          title: "Transformer Model Architecture",
+        
+        description: "From encoders to masked attention and decoders - Transformer architecture demystified.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/transformer-architecture/";
+          
+        },
+      },{id: "post-birth-of-attention-mechanism",
+        
+          title: "Birth of Attention Mechanism",
+        
+        description: "The evolution of attention in sequence-to-sequence models.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/birth-of-attention-mechanism/";
+          
+        },
+      },];
