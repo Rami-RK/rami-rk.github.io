1b0cd439de67fafac5e7a4bbd5a4acad2bdf310f
Rami-RK <50700579+Rami-RK@users.noreply.github.com>
2026-02-21 18:24:07 +0000

    Deploying to gh-pages from @ Rami-RK/rami-rk.github.io@9e16a9e350fee2253411ec68139e2495859aecbe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,51 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-self-attention",
+        },{id: "post-continuous-distribution-amp-pdf",
+        
+          title: "Continuous Distribution &amp; PDF",
+        
+        description: "From discrete outcomes to continuous intervals - understanding probability density functions.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/continuous-distribution/";
+          
+        },
+      },{id: "post-normal-distribution",
+        
+          title: "Normal Distribution",
+        
+        description: "From Histogram to Normal Distribution.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/normal-distribution/";
+          
+        },
+      },{id: "post-random-variables",
+        
+          title: "Random Variables",
+        
+        description: "Building the intuition of random variables, starting with a coin toss.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/random-variables/";
+          
+        },
+      },{id: "post-understanding-derivatives",
+        
+          title: "Understanding Derivatives",
+        
+        description: "Building intuition and physical meaning of derivatives with simple examples.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/understanding-derivatives/";
+          
+        },
+      },{id: "post-self-attention",
         
           title: "Self Attention",
         
